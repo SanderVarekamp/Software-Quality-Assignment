13c2726fdde3b5ab5c8df6bd35491d02857faf06
SanderVarekamp <sanderv2004@hotmail.com>
2024-05-28 12:05:44 +0200

    Made Employee class and tested database

---
+++ Employee.py
@@ -0,0 +1,72 @@
+import datetime
+import random
+import sqlite3
+
+class Employee:
+    def __init__(self, FirstName, LastName, Age, Gender, Weight, Address, City, Email, PhoneNumb, IsConsult, IsSysAdm, IsSupAdm):
+        self.FirstName = FirstName
+        self.LastName = LastName
+        self.Age = Age
+        self.Gender = Gender
+        self.Weight = Weight
+        self.Address = Address
+        self.City = City
+        self.Email = Email
+        self.PhoneNumb = PhoneNumb
+        self.IsConsult = IsConsult
+        self.IsSysAdm = IsSysAdm
+        self.IsSupAdm = IsSupAdm
+        self.RegistrationDate = datetime.date.today()
+        self.MemberID = self.GenerateMemberID()
+
+    def GenerateMemberID(self):
+        Numb0 = int(str(self.RegistrationDate)[2])
+        Numb1 = int(str(self.RegistrationDate)[3])
+        Numb2 = random.randint(0, 9)
+        Numb3 = random.randint(0, 9)
+        Numb4 = random.randint(0, 9)
+        Numb5 = random.randint(0, 9)
+        Numb6 = random.randint(0, 9)
+        Numb7 = random.randint(0, 9)
+        Numb8 = random.randint(0, 9)
+        Numb9 = (Numb0 + Numb1 + Numb2 + Numb3 + Numb4 + Numb5 + Numb6 + Numb7 + Numb8) % 10
+        return int(str(Numb0) + str(Numb1) + str(Numb2) + str(Numb3) +
+                   str(Numb4) + str(Numb5) + str(Numb6) + str(Numb7) + str(Numb8) + str(Numb9))
+
+    def InsertData(self):
+        connection = sqlite3.connect("members.db")
+        cursor = connection.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS Members (
+                            FirstName TEXT, 
+                            LastName TEXT, 
+                            Age INTEGER, 
+                            Gender TEXT, 
+                            Weight INTEGER, 
+                            Address TEXT, 
+                            City TEXT, 
+                            Email TEXT, 
+                            PhoneNumber TEXT, 
+                            IsConsult BOOLEAN, 
+                            IsSysAdm BOOLEAN, 
+                            IsSupAdm BOOLEAN, 
+                            RegistrationDate TEXT, 
+                            MemberID INTEGER
+                          )""")
+        cursor.execute("INSERT INTO Members VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                       (self.FirstName, self.LastName, self.Age, self.Gender, self.Weight, 
+                        self.Address, self.City, self.Email, self.PhoneNumb, self.IsConsult, 
+                        self.IsSysAdm, self.IsSupAdm, str(self.RegistrationDate), self.MemberID))
+        connection.commit()
+        connection.close()
+
+    def Print(self):
+        print(self.FirstName, self.LastName)
+        print(self.Age)
+        print(self.Gender)
+        print(self.Weight)
+        print(self.Address, self.City)
+        print(self.Email)
+        print(self.PhoneNumb)
+        print(self.IsConsult)
+        print(self.IsSysAdm)
+        print(self.IsSupAdm)
--- Main.py
+++ Main.py
@@ -1,4 +1,16 @@
 import sqlite3
 
+from Employee import Employee
+
+class main:
+    def start():
+        print ("Hello world!")
+        E1 = Employee("Emma", "Doe", 36, "Female", 70, 
+                      "Wijnhaven 107 3232KL", "Rotterdam", "Emma.Doe@Test.com", "0653458917", True, False, False)
+        E1.Print()
+        print(E1.RegistrationDate)
+        print(E1.MemberID)
+        E1.InsertData()
+
 if __name__ == '__main__':
-    print("Hello world!")
+    main.start()
